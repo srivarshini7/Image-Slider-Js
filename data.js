da976ec1a05db9f5f7f180013b38c3dfa598bf28
srivarshini7 <srivarshinirao7@gmail.com>
2024-05-02 21:10:02 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,41 @@
+const people = [
+    {
+      img: 'https://poets.org/sites/default/files/images/biographies/oscar_wilde.png',
+      name: 'Oscar Wilde',
+      job: 'Author',
+      text: `Our ambition should be to rule ourselves, the true kingdom for each one of us; and true progress is to know more, and be more, and to do more.`,
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/3/3a/CBRichmond.png',
+      name: 'Charlotte Bronte',
+      job: 'Author',
+      text: `I am no bird; and no net ensnares me: I am a free human being with an independent will.`,
+    },
+    {
+      img: 'https://calvinrosser.com/wp-content/uploads/2024/01/bukowski-2.png',
+      name: 'Charles Bukowski',
+      job: 'Author',
+      text: `Sometimes you climb out of bed in the morning and you think, I'm not going to make it, but you laugh inside — remembering all the times you've felt that way.`,
+    },
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIx7r9lVSFgPW5uz-jqkY9GbjVLU4g2N8sxwU_tj1ehS3X8SQv',
+      name: 'Heler Keller',
+      job: 'Author',
+      text: `No pessimist ever discovered the secret of the stars, or sailed to an uncharted land, or opened a new doorway for the human spirit.`,
+    },
+    {
+      img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQ3xpmlvrkzSPtDiUKLwG4Ut4C9srTCfqFJfDw1ldN3YiwzVFK9',
+      name: 'Jane Austen',
+      job: 'Author',
+      text: `Give a girl an education and introduce her properly into the world, and ten to one but she has the means of settling well, without further expense to anybody.`,
+    },
+    {
+      img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSO9tlvFnCt_mW87_X4sJ5y8WTMwp6sU_4_tOqQVV-3Kiov-s4P',
+      name: 'H.P LoveCraft',
+      job: 'Author',
+      text: `I couldn't live a week without a private library - indeed, I'd part with all my furniture and squat and sleep on the floor before I'd let go of the 1500 or so books I possess.`,
+    }
+    
+  ];
+  
+  export default people;
